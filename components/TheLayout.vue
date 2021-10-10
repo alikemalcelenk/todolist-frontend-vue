92ead5c54cb472a5276f8cec8b7f6cef8365cf27
Ali Kemal Çelenk <alikemalcelenkk@gmail.com>
2021-10-10 18:51:18 +0300

    start to create pages designs & create layout comp

---
+++ components/TheLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <Box class="root">
+    <slot />
+  </Box>
+</template>
+
+<style lang="scss" scoped>
+.root {
+  height: 100vh;
+  display: grid;
+  grid-template-rows: 80px 1fr 80px;
+  min-width: 300px;
+}
+</style>
---
+++ pages/home.vue
@@ -0,0 +1,7 @@
+<template>
+  <TheLayout>
+    <Box style="background-color: gray" />
+    <Box style="background-color: blue" />
+    <Box style="background-color: green" />
+  </TheLayout>
+</template>
